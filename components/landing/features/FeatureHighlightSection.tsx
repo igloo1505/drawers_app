4be94faffac30091ffcf4ca14603b9650a411888
igloo1505 <iglinskia44@gmail.com>
2023-06-01 20:38:47 -0500

    Some more minor alignment issues that really only affect mobile devices.

--- components/landing/features/FeatureHighlightSection.tsx
+++ components/landing/features/FeatureHighlightSection.tsx
@@ -18,7 +18,7 @@ const iconMap = {
 
 const FeaturedHighlightTitleGroup = ({ subtitle, title }: { subtitle: string, title: string }) => {
     return (
-        <div className='w-full pt-12 flex flex-col justify-center items-center gap-3'>
+        <div className='w-full pt-12 flex flex-col justify-center items-center gap-3 text-center'>
             <div className={'text-4xl font-bold'}>{title}</div>
             <div className={'text-xl text-center mb-4'} style={{
                 maxWidth: "min(max(85vw, calc(100vw - 4rem)), 768px)"
@@ -29,7 +29,7 @@ const FeaturedHighlightTitleGroup = ({ subtitle, title }: { subtitle: string, ti
 
 const FeatureHighlightSection = ({ highlightedFeatures, featuredSectionDetails }: FeatureHighlightSectionProps) => {
     return (
-        <div className={'bg-[--highlight-bg] mt-8'}>
+        <div className={'bg-[--highlight-bg] mt-8 px-2'}>
             <FeaturedHighlightTitleGroup title={featuredSectionDetails.title} subtitle={featuredSectionDetails.subtitle} />
             <div className={'w-full grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-4 lg:grid-rows-1 gap-4 pt-14 pb-8 px-6  responsive-row-gap-highlighted mt-6'} >
                 {highlightedFeatures.map((f, i) => {

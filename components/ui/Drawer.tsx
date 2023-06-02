23b48ae3c51d7130b7cdeafe978356da9909046a
igloo1505 <iglinskia44@gmail.com>
2023-06-02 12:46:35 -0500

    Added clickoutside hook to drawer.

--- components/ui/Drawer.tsx
+++ components/ui/Drawer.tsx
@@ -1,10 +1,11 @@
 "use client"
-import React from 'react'
+import React, { useRef } from 'react'
 import { unAuthenticatedButtons } from '../navigation/NavbarButtonSection';
 import DrawerButton from './DrawerButton';
 import { connect } from 'react-redux';
 import { RootState } from '../../state/store';
 import { toggleDrawer } from '../../state/actions/syncActions';
+import { useClickOutside } from 'primereact/hooks'
 
 const connector = connect((state: RootState, props: any) => ({
     drawerOpen: state.UI.drawerOpen,
@@ -18,11 +19,20 @@ interface DrawerProps {
 
 
 const Drawer = connector(({ drawerOpen }: DrawerProps) => {
+    const ref = useRef<HTMLDivElement>(null!)
+    useClickOutside(ref, () => {
+        if (drawerOpen) {
+            toggleDrawer(false)
+        }
+    }
+    )
     return (
         <div className={'h-screen fixed top-0 left-0 w-fit max-w-[60vw] min-w-[150px] z-[100] bg-[--surface-800] text-white px-4 flex flex-col justify-start items-start gap-2 transition-transform duration-500'}
             style={{
                 transform: drawerOpen ? "translateX(0)" : "translateX(-100%)"
-            }}>
+            }}
+            ref={ref}
+        >
             <div className={'w-full flex flex-row justify-end items-start mt-4 mb-4 cursor-pointer'}><i className={'pi pi-times'} onClick={() => toggleDrawer(false)} /></div>
             {unAuthenticatedButtons.map((b, i) => {
                 return <DrawerButton item={b} key={`drawer-button-${i}`} />
